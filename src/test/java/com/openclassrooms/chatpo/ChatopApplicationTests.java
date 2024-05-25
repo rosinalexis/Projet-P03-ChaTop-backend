c9edd4af693cc6ee427fd6fcd6d0a08c003d0f63
rosinalexis <rosinalexis@yahoo.fr>
2024-05-25 21:37:29 +0200

    first commit

---
+++ src/test/java/com/openclassrooms/chatpo/ChatopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.chatpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
