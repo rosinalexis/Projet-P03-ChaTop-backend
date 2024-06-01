394b63e292016a0dc131473ad08848204435003b
rosinalexis <rosinalexis@yahoo.fr>
2024-06-01 20:30:34 +0200

    P03/Models : ajout du controlleur de message

--- src/main/java/com/openclassrooms/chatpo/controllers/MessageController.java
+++ src/main/java/com/openclassrooms/chatpo/controllers/MessageController.java
@@ -3,11 +3,15 @@ package com.openclassrooms.chatpo.controllers;
 import com.openclassrooms.chatpo.dto.MessageDto;
 import com.openclassrooms.chatpo.services.MessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/messages")
@@ -17,9 +21,15 @@ public class MessageController {
     private final MessageService messageService;
 
     @PostMapping
-    public ResponseEntity<Integer> save(
+    public ResponseEntity<Map<String, String>> save(
             @RequestBody MessageDto messageDto
     ) {
-        return ResponseEntity.ok(messageService.save(messageDto));
+
+        Map<String, String> response = new HashMap<>();
+
+        if (messageService.save(messageDto) > 0) {
+            response.put("message", "Message send with success");
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
